485143a2a01ffcfeaacb6b784e1245e233cc0db7
Shayan Husaini <shusaini@csquareonline.com>
2019-04-24 22:10:41 +0500

    Testing form render

--- web/index.php
+++ web/index.php
@@ -1,13 +1,17 @@
 <?php
 require '../vendor/autoload.php';
-use Symfony\Component\HttpFoundation\Request;
-use NewTwitchApi\HelixGuzzleClient;
 use GuzzleHttp\Client;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 $app = new Silex\Application();
 $app['debug'] = true;
 //Register session service
 $app->register(new Silex\Provider\SessionServiceProvider());
+
+//Register form service
+$app->register(new FormServiceProvider());
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
@@ -21,21 +25,27 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 // Our web handlers
 $app->get('/', function (Request $request) use ($app) {
     $app['monolog']->addDebug('logging output.');
+    $is_logged_in = false;
     $params = $request->query->all();
     if (isset($params['code'])) {
         $guzzleClient = new Client();
-        $uri = 'https://id.twitch.tv/oauth2/token?client_id='.getenv('TWICH_CLIENT_ID').'&client_secret='.getenv('TWICH_CLIENT_SECRET').'&code='.$params['code'].'&grant_type=authorization_code&redirect_uri='.getenv('TWICH_REDIRECT_URI');
+        $uri = 'https://id.twitch.tv/oauth2/token?client_id=' . getenv('TWICH_CLIENT_ID') . '&client_secret=' . getenv('TWICH_CLIENT_SECRET') . '&code=' . $params['code'] . '&grant_type=authorization_code&redirect_uri=' . getenv('TWICH_REDIRECT_URI');
         $response = $guzzleClient->post($uri, []);
         $app['session']->set('twich_token', (string) $response->getBody());
-        echo '<pre>';
-        print_r($app['session']->get('twich_token'));
-        echo '</pre>';
+        $is_logged_in = true;
     }
+
+    $form = $app['form.factory']->createBuilder(FormType::class, ['name' => 'Streamer Username'])
+            ->add('name')
+            ->add('submit', SubmitType::class, ['label' => 'Save'])
+            ->getForm();
 
     return $app['twig']->render('index.twig', [
         'client_id' => getenv('TWICH_CLIENT_ID'),
         'redirect_url' => getenv('TWICH_REDIRECT_URI'),
         'scope' => 'user_read',
+        'is_logged_in' => $is_logged_in,
+        'form' => $form->createView()
     ]);
 });
 $app->run();
